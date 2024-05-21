678b67a793d4b2b44a1a1341b25e0a6e05646284
Jason <46291780+JasonMorta@users.noreply.github.com>
2024-05-21 21:18:14 +0200

    updates

--- controllers/accountSummaryUsers.js
+++ controllers/accountSummaryUsers.js
@@ -1,8 +1,8 @@
-const {generatedUsers} = require('./generateUsers');
+
 
 exports.accountSummaryUsers = async (req, res) => {
   console.log('processing request for accountSummaryUsers')
-
+  const { generatedUsers } = require('./generateUsers');
     console.log('Sending users to the client',  generatedUsers.length)
       res.json( generatedUsers);
       console.log('Request processed successfully!')
--- controllers/generateUsers.js
+++ controllers/generateUsers.js
@@ -4,19 +4,20 @@ const from = "2000/1/1";
 const to = "2024/4/3";
 
 const createRandomUser = () => {
+  const formatting = { min: 0, max: 1000, dec: 2, symbol: '', autoFormat: true }
   return {
     // summary table
-    login: faker.string.numeric(7),
-    deposit: parseFloat(faker.finance.amount()),
-    withdraw: parseFloat(faker.finance.amount()),
-    inOut: parseFloat(faker.finance.amount()),
-    credit: parseFloat(faker.finance.amount()),
-    additional: parseFloat(faker.finance.amount()),
-    commission: parseFloat(faker.finance.amount()),
-    fee: parseFloat(faker.finance.amount()),
-    currentBalance: parseFloat(faker.finance.amount()),
-    currentEquity: parseFloat(faker.finance.amount()),
-    margin: parseFloat(faker.finance.amount()),
+    login: faker.string.numeric(5),
+    deposit: parseFloat(faker.finance.amount(formatting)),
+    withdraw: parseFloat(faker.finance.amount(formatting)),
+    inOut: parseFloat(faker.finance.amount(formatting)),
+    credit: parseFloat(faker.finance.amount(formatting)),
+    additional: parseFloat(faker.finance.amount(formatting)),
+    commission: parseFloat(faker.finance.amount(formatting)),
+    fee: parseFloat(faker.finance.amount(formatting)),
+    currentBalance: parseFloat(faker.finance.amount(formatting)),
+    currentEquity: parseFloat(faker.finance.amount(formatting)),
+    margin: parseFloat(faker.finance.amount(formatting)),
     currency: faker.finance.currencyCode(),
 
     // overview tab
@@ -37,17 +38,48 @@ const createRandomUser = () => {
     countries: ['USA', 'Canada', 'SA'],
     company: faker.company.name(),
     id: faker.string.uuid(),
-
-    // running trades
-    symbol: 'EURUSD',
-    stopLoss: parseFloat(faker.finance.amount()),
-    swaps: parseFloat(faker.finance.amount()),
-    volume: parseFloat(faker.finance.amount()),
-    profit: parseFloat(faker.finance.amount()),
-    takeProfit: parseFloat(faker.finance.amount()),
-    type: 'Buy',
-    openPrice: parseFloat(faker.finance.amount()),
-    price: parseFloat(faker.finance.amount())
+    runningTrades: [
+      {
+        symbol: 'EURUSD',
+        stopLoss: parseFloat(faker.finance.amount(formatting)),
+        swaps: parseFloat(faker.finance.amount(formatting)),
+        volume: parseFloat(faker.finance.amount(formatting)),
+        profit: parseFloat(faker.finance.amount(formatting)),
+        takeProfit: parseFloat(faker.finance.amount(formatting)),
+        type: 'Buy',
+        openPrice: parseFloat(faker.finance.amount(formatting)),
+        price: parseFloat(faker.finance.amount(formatting))
+      },
+      {
+        symbol: 'USDJPY',
+        stopLoss: parseFloat(faker.finance.amount(formatting)),
+        swaps: parseFloat(faker.finance.amount(formatting)),
+        volume: parseFloat(faker.finance.amount(formatting)),
+        profit: parseFloat(faker.finance.amount(formatting)),
+        takeProfit: parseFloat(faker.finance.amount(formatting)),
+        type: 'Sell',
+        openPrice: parseFloat(faker.finance.amount(formatting)),
+        price: parseFloat(faker.finance.amount(formatting))
+      },
+      {
+        symbol: 'GBPUSD',
+        stopLoss: parseFloat(faker.finance.amount(formatting)),
+        swaps: parseFloat(faker.finance.amount(formatting)),
+        volume: parseFloat(faker.finance.amount(formatting)),
+        profit: parseFloat(faker.finance.amount(formatting)),
+        takeProfit: parseFloat(faker.finance.amount(formatting)),
+        type: 'Buy',
+        openPrice: parseFloat(faker.finance.amount(formatting)),
+        price: parseFloat(faker.finance.amount(formatting))
+      }
+    ],
+    accountInfo: {
+      balance: parseFloat(faker.finance.amount(formatting)),
+      equity: parseFloat(faker.finance.amount(formatting)),
+      margin: parseFloat(faker.finance.amount(formatting)),
+      freeMargin: parseFloat(faker.finance.amount(formatting)),
+      marginLevel: parseFloat(faker.finance.amount(formatting))
+    }
   };
 };
 
@@ -85,15 +117,22 @@ const generateUsersWithTotals = (count) => {
     totals.currentBalance += user.currentBalance;
     totals.currentEquity += user.currentEquity;
     totals.margin += user.margin;
-    totals.swaps += user.swaps;
-    totals.volume += user.volume;
-    totals.profit += user.profit;
+    totals.swaps += user.runningTrades.reduce((acc, trade) => acc + trade.swaps, 0);
+    totals.volume += user.runningTrades.reduce((acc, trade) => acc + trade.volume, 0);
+    totals.profit += user.runningTrades.reduce((acc, trade) => acc + trade.profit, 0);
+  }
+
+  // Fix totals to 2 decimal places
+  for (let key in totals) {
+    if (totals.hasOwnProperty(key)) {
+      totals[key] = parseFloat(totals[key].toFixed(2));
+    }
   }
 
   return { users, totals };
 };
 
-const generatedUsers =  { users: users, totals: totals } = generateUsersWithTotals(10000);
+const generatedUsers = generateUsersWithTotals(50000);
 
 module.exports = {
   generatedUsers
